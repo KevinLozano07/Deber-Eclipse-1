82b891750a0008245dde0350106f3bb6a7735adc
KevinLozano07 <kevin.lozano@tecnologicoliceocristiano.edu.ec>
2024-12-22 08:22:54 -0500

    Deber numero 1

---
+++ Programacion_orientada_objetos/src/Pack1/Consola.java
@@ -0,0 +1,10 @@
+package Pack1;
+
+public class Consola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Programacion_orientada_objetos/src/Pack1/Foco.java
@@ -0,0 +1,26 @@
+package Pack1;
+
+public class Foco {
+	public int Potencia;
+	public String Tipo_Luz;
+	public String Color_Luz;
+	public int Duracion;
+	public int Voltage;
+	public String Marca;
+	public boolean RGB;
+	public String Certificacion;
+	public String Material_cuerpo;
+	public String Material_base;
+	public String Base;
+	public String Forma;
+	public int Lumenes;
+	public int Ahorro;
+	public boolean Detector_movimiento;
+	public boolean Control_remoto;
+	public String Funciones_especiales;
+	public boolean Recagable;
+	public int Grados;
+	public int Numero_serie; //Identificador unico
+	
+	
+}
